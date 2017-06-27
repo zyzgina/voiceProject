863bc56e52826b95a1b476b66763987aa9588542
左丽姬 <zyzgina@163.com>
2017-06-27 11:40:17 +0800

    完成监听

--- app/src/main/java/com/reliablel/voiceproject/VoiceUtils.java
+++ app/src/main/java/com/reliablel/voiceproject/VoiceUtils.java
@@ -50,7 +50,7 @@ public class VoiceUtils {
         // 发音人（在线引擎），可用参数为0,1,2,3。。。（服务器端会动态增加，各值含义参考文档，以文档说明为准。0--普通女声，1--普通男声，2--特别男声，3--情感男声。。。）
         this.mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_SPEAKER, "0");
         //语速设置
-        this.mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_SPEED, "9");
+        this.mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_SPEED, "6");
         // 设置Mix模式的合成策略
         this.mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_MIX_MODE, SpeechSynthesizer.MIX_MODE_DEFAULT);
         // 授权检测接口(只是通过AuthInfo进行检验授权是否成功。)
